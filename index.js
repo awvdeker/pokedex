6c3e919cb958a5188a5313519f132ed481597bf6
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-03 17:06:07 +0200

    id and image: ok; no styling yet!

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-var pokemonName = document.getElementById('pokemonName');
+var pokedex = document.getElementById('pokedex');
+var pokemonNameInput = document.getElementById('pokemonNameInput');
 var searchButton = document.getElementById('searchButton');
+var pokemonName = document.getElementById('pokemonName');
+var pokemonId = document.getElementById('pokemonId');
+var imageHolder = document.getElementById('imageHolder');
 
 searchButton.addEventListener('click', function(){
-  searching (pokemonName.value);
+  searching (pokemonNameInput.value);
 });
 
 
 function searching (pokename) {
-
   var xhr = new XMLHttpRequest();
 
   xhr.onload = function(){
     if(xhr.status === 200){
-      responseObject = JSON.parse(xhr.responseText);
-      //for (let i = 0 ; i<responseObject.results.length ; i++){
+      var pokemonObject = JSON.parse(xhr.responseText);
+      var found = false;
+      for (let i = 0 ; i<pokemonObject.results.length ; i++){
 
-        //if (){break;}
-      //}
-      document.getElementById('pokedex').innerHTML += "Pokemon Name: "+responseObject.results[1].name;
+        if (pokemonObject.results[i].name == pokename){
+          found = true;
+          var url = pokemonObject.results[i].url;
+          break;
+        }
+      }
+      if (!found) {
+        pokemonName.innerHTML = 'Not found!';
+        pokemonId.innerHTML = "";
+        imageHolder.innerHTML = "";
+      } else {
+        pokemonName.innerHTML = "Pokemon name: "+ pokename;
+
+        //nieuwe call om id nummer te weten en picture
+        var xhr2 = new XMLHttpRequest();
+
+        xhr2.onload = function(){
+          if (xhr2.status === 200){
+            var pokemonTypeObject = JSON.parse(xhr2.responseText);
+            pokemonId.innerHTML = pokemonTypeObject.id;
+            var picture = document.createElement('img');
+            picture.src=pokemonTypeObject.sprites.front_default;
+            picture.height="200";
+            picture.width="200";
+            imageHolder.appendChild(picture);
+
+          }
+        }
+
+        xhr2.open('GET',url,true);
+        xhr2.send(null);
+
+      }
     }
   };
 
