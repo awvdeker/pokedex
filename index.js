d665e5e3d4278a1492935785cce0d61f99f521ab
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-06 16:45:17 +0200

    previous evolution with a picture

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var pokemonName = document.getElementById('pokemonName');
 var pokemonId = document.getElementById('pokemonId');
 var imageHolder = document.getElementById('imageHolder');
 var pokemonMoves = document.getElementById('pokemonMoves');
+var previousEvolution = document.getElementById('previousEvolution');
+var previousEvolutionText = document.getElementById('previousEvolutionText');
+var previousEvolutionImage = document.getElementById('previousEvolutionImage');
 
 searchButton.addEventListener('click', function(){
   searching (pokemonNameInput.value);
@@ -31,6 +34,8 @@ function searching (pokename) {
         pokemonId.innerHTML = "";
         imageHolder.innerHTML = "";
         pokemonMoves.innerHTML = "";
+        previousEvolutionText.innerHTML = "";
+        previousEvolutionImage.innerHTML = "";
       } else {
         pokemonName.innerHTML = "Pokemon name: "+ pokename;
 
@@ -40,8 +45,12 @@ function searching (pokename) {
         xhr2.onload = function(){
           if (xhr2.status === 200){
             var pokemonTypeObject = JSON.parse(xhr2.responseText);
+
+            //id
             pokemonId.innerHTML = "ID: " + pokemonTypeObject.id;
 
+
+            //picture
             if (document.getElementById("shownPicture")){
               pokemonImageRemove=document.getElementById("shownPicture");
               imageHolder.removeChild(pokemonImageRemove);
@@ -54,12 +63,73 @@ function searching (pokename) {
             picture.width="200";
             imageHolder.appendChild(picture);
 
+
+            //4 moves
             pokemonMoves.innerHTML = "";
             for (let i = 0 ; i < 4 ; i++){
               pokemonMoves.innerHTML += "move "+ (i+1) + ": " + pokemonTypeObject.moves[i].move.name + "<br/>";
             }
+
+            //previous evolution
+            var xhr3 = new XMLHttpRequest();
+
+            xhr3.onload = function(){
+              if(xhr3.status === 200){
+                var evolutionObject = JSON.parse(xhr3.responseText);
+                if (evolutionObject.evolves_from_species) {
+                  previousEvolutionText.innerHTML = "Previous evolution: " + evolutionObject.evolves_from_species.name;
+
+                  //picture of previous evolution
+
+                  var xhr4 = new XMLHttpRequest();
+
+                  xhr4.onload = function() {
+                    if(xhr4.status === 200){
+                      var evolution2Object = JSON.parse(xhr4.responseText);
+
+                      //nieuwe call
+                      var xhr5 = new XMLHttpRequest();
+
+                      xhr5.onload = function() {
+                        if(xhr5.status === 200){
+                          var pokemonPictureObject = JSON.parse(xhr5.responseText);
+
+                          //showing the previous picture
+                          if (document.getElementById("previousPicture")){
+                            pokemonImageRemove2=document.getElementById("previousPicture");
+                            previousEvolutionImage.removeChild(pokemonImageRemove2);
+                          }
+
+                          var picture2 = document.createElement('img');
+                          picture2.id="previousPicture";
+                          picture2.src=pokemonPictureObject.sprites.front_default;
+                          picture2.height="200";
+                          picture2.width="200";
+                          previousEvolutionImage.appendChild(picture2);
+
+                        }
+                      };
+                      xhr5.open('GET',evolution2Object.varieties[0].pokemon.url,true);
+                      xhr5.send(null);
+
+                    }
+                  };
+                  xhr4.open('GET',evolutionObject.evolves_from_species.url,true);
+                  xhr4.send(null);
+
+                } else {
+                  previousEvolutionText.innerHTML = "No previous evolution!";
+                  if (document.getElementById("previousPicture")){
+                    pokemonImageRemove3=document.getElementById("previousPicture");
+                    previousEvolutionImage.removeChild(pokemonImageRemove3);
+                  }
+                }
+              }
+            };
+            xhr3.open('GET',pokemonTypeObject.species.url,true);
+            xhr3.send(null);
           }
-        }
+        };
 
         xhr2.open('GET',url,true);
         xhr2.send(null);
